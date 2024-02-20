3e9611da750ffd09a121ecf9fe4a189429f3c7e9
dungatoro <noahjbarker@icloud.com>
2024-02-20 14:20:48 +0000

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Graphy
+
+- IDs are nodes in a graph
+  - IDs are primary keys into table of relevant data
+
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,27 @@ mod args;
 use args::GraphyArgs;
 use clap::Parser;
 
+struct Edge {
+    node: Node,
+    label: String,
+}
+
 struct Node {
     // id is a unique identifier into DB of items
     id: String,
-    edges: Vec<Node>
+    edges: Vec<Edge>,
 }
 
 impl Node {
     fn new(id: String) -> Self {
-        Self { id, edges: Vec::new() }
+        Self {
+            id,
+            edges: Vec::new(),
+        }
     }
 }
 
 fn main() {
-    
+    // let args = GraphyArgs::parse();
+    // let path = args.db_path;
 }
